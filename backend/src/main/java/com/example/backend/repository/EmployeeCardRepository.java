8424174bd4eb0fee53d750ff65e6c5ffe8561bc7
Abhyudita Singh <singhabhyudita@gmail.com>
2023-08-16 13:14:56 +0530

    loan and item done

--- backend/src/main/java/com/example/backend/repository/EmployeeCardRepository.java
+++ backend/src/main/java/com/example/backend/repository/EmployeeCardRepository.java
@@ -1,5 +1,7 @@
 package com.example.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.example.backend.model.EmployeeCard;
 
 @Repository
 public interface EmployeeCardRepository extends JpaRepository<EmployeeCard,String> {
+	public List<EmployeeCard> findByEmployeeId(String employeeId);
 }
--- backend/src/main/java/com/example/backend/service/LoanService.java
+++ backend/src/main/java/com/example/backend/service/LoanService.java
@@ -1,7 +1,29 @@
 package com.example.backend.service;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.backend.model.LoanAvailed;
+import com.example.backend.repository.EmployeeCardRepository;
+import com.example.backend.repository.LoanRepository;
+
+@Service
 public class LoanService {
-
+	
+	@Autowired
+	EmployeeCardRepository employeeCardRepository;
+	
+	@Autowired
+	LoanRepository loanRepository;
+	
+	public List<LoanAvailed> getItemsById(String id) {
+		List<LoanAvailed> list = new ArrayList<>();
+		employeeCardRepository.findByEmployeeId(id).forEach((obj)-> list.add(new LoanAvailed(loanRepository.findById(obj.getLoanId()).get(),obj.getCard_issue_date())));
+		return list;
+		
+	}
+	
 }

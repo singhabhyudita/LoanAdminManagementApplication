5db9940a30472b6a106e75f67d98e9717be46a48
ruujuutaa <palimkarrujuta@gmail.com>
2023-08-17 15:02:24 +0530

    testing

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages="com")
+@SpringBootTest(classes= {com.example.backend.EmployeeControllerTest.class, com.example.backend.LoanControllerTest.class, com.example.backend.AdminControllerTest.class, com.example.backend.ItemControllerTest.class})
 
-@SpringBootTest
 class BackendApplicationTests {
 
 	@Test

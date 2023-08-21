f1c261fa606faad488ab826e027763e70426cc60
ruujuutaa <palimkarrujuta@gmail.com>
2023-08-21 16:17:35 +0530

    exception

--- backend/src/main/java/com/example/backend/exception/NoDataFoundException.java
+++ backend/src/main/java/com/example/backend/exception/NoDataFoundException.java
@@ -1,5 +1,20 @@
 package com.example.backend.exception;
 
-public class NoDataFoundException {
+public class NoDataFoundException extends Exception {
+	private String message;
 
+	public NoDataFoundException(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
 }
